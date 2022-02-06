7234d80f21d7719ed794d77f10ae95ddceaedd60
Williamoditi <williams.oditi@student.moringaschool.com>
2022-02-06 11:48:18 +0300

    chore : Result on same page - Optimizes page to show result on the same page

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function check() {
-   var a=0;
+   var score = 0;
    var q1=document.quiz.question1.value;
    var q2=document.quiz.question2.value;
    var q3=document.quiz.question3.value;
-   if (q1= "HTML") {a++}
-   if (q1= "JS") {a++}
-   if (q1= "No") {a++}
-
-       document.write(a)
+   var outcome= document.getElementById("result")
+   if (q1=="HTML") {score++}
+   if (q2=="JS") {score++}
+   if (q3=="No") {score++}
+   outcome.textContent = score;
 }
