5e5bdddf7251ee38841672a3de10c83b05e18873
Williamoditi <williams.oditi@student.moringaschool.com>
2022-02-06 09:07:54 +0300

    chore: Total tally- Adds the total tally for the three questions

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function check() {
    var a=0;
    var q1=document.quiz.question1.value;
+   var q2=document.quiz.question2.value;
+   var q3=document.quiz.question3.value;
    if (q1= "HTML") {a++}
+   if (q1= "JS") {a++}
+   if (q1= "No") {a++}
+
        document.write(a)
 }
